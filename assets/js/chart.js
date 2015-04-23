eda33fddf246c7280cb56cdf9119161eb9c9e83c
runbow <dragoon.redeyed@gmail.com>
2015-04-23 18:56:00 +0700

    data modus

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -152,19 +152,14 @@ var modusOption = {
     yAxis : [
         {
             type : 'category',
-            data : ['巴西','印尼','美国','印度','中国','世界人口(万)']
+            data : ['Anggaran Ganda','Pungutan Liar','Suap','Kegiatan Fiktif','Pemotongan Anggaran','Penyalahgunaan Wewenang','Mark up','Laporan Fiktif','Penggelapan','Penyalahgunaan Anggaran']
         }
     ],
     series : [
         {
-            name:'2011年',
+            //name:'2011年',
             type:'bar',
-            data:[18203, 23489, 29034, 104970, 131744, 630230]
-        },
-        {
-            name:'2012年',
-            type:'bar',
-            data:[19325, 23438, 31000, 121594, 134141, 681807]
+            data:[1, 8, 11, 12, 15, 20,33,66,71,71]
         }
     ]
 };
