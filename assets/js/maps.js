3e325a31f81a0515d6c8366f082c4f11183e185f
Ichlasul Amal <ichlasul.a@gmail.com>
2015-04-23 16:20:51 +0700

    Remove Leaflet

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,120 +1,164 @@
+require.config({
+    packages: [
+        {
+            name: 'echarts',
+            location: '//ecomfe.github.io/echarts/src',
+            main: 'echarts'
+        },
+        {
+            name: 'zrender',
+            location: '//ecomfe.github.io/zrender/src',
+            main: 'zrender'
+        }
+    ]
+});
 
-var map = L.map('map-leaflet').setView([37.8, -96], 4);
-
-L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-		'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	id: 'examples.map-20v6611k'
-}).addTo(map);
-
-
-// control that shows state info on hover
-var info = L.control();
-
-info.onAdd = function (map) {
-	this._div = L.DomUtil.create('div', 'info');
-	this.update();
-	return this._div;
+var option = {
+    title : {
+        text: 'USA Population Estimates (2012)',
+        subtext: 'Data from www.census.gov.',
+        sublink: 'http://www.census.gov/popest/data/datasets.html',
+        x:'right'
+    },
+    tooltip : {
+        trigger: 'item',
+        showDelay: 0,
+        transitionDuration: 0.2,
+        formatter : function (params) {
+            var value = (params.value + '').split('.');
+            value = value[0].replace(/(\d{1,3})(?=(?:\d{3})+(?!\d))/g, '$1,');
+            return params.seriesName + '<br/>' + params.name + ' : ' + value;
+        }
+    },
+    dataRange: {
+        x : 'right',
+        min: 500000,
+        max: 38000000,
+        color: ['orangered','yellow','lightskyblue'],
+        text:['High','Low'],           // 文本，默认为数值文本
+        calculable : true
+    },
+    toolbox: {
+        show : true,
+        //orient : 'vertical',
+        x: 'left',
+        y: 'top',
+        feature : {
+            mark : {show: true},
+            dataView : {show: true, readOnly: false},
+            restore : {show: true},
+            saveAsImage : {show: true}
+        }
+    },
+    series : [
+        {
+            name: 'USA PopEstimates',
+            type: 'map',
+            roam: true,
+            mapType: 'USA', // 自定义扩展图表类型
+            itemStyle:{
+                emphasis:{label:{show:true}}
+            },
+            // 文本位置修正
+            textFixed : {
+                Alaska : [20, -20]
+            },
+            data:[
+                {name : 'Alabama', value : 4822023},
+                {name : 'Alaska', value : 731449},
+                {name : 'Arizona', value : 6553255},
+                {name : 'Arkansas', value : 2949131},
+                {name : 'California', value : 38041430},
+                {name : 'Colorado', value : 5187582},
+                {name : 'Connecticut', value : 3590347},
+                {name : 'Delaware', value : 917092},
+                {name : 'District of Columbia', value : 632323},
+                {name : 'Florida', value : 19317568},
+                {name : 'Georgia', value : 9919945},
+                {name : 'Hawaii', value : 1392313},
+                {name : 'Idaho', value : 1595728},
+                {name : 'Illinois', value : 12875255},
+                {name : 'Indiana', value : 6537334},
+                {name : 'Iowa', value : 3074186},
+                {name : 'Kansas', value : 2885905},
+                {name : 'Kentucky', value : 4380415},
+                {name : 'Louisiana', value : 4601893},
+                {name : 'Maine', value : 1329192},
+                {name : 'Maryland', value : 5884563},
+                {name : 'Massachusetts', value : 6646144},
+                {name : 'Michigan', value : 9883360},
+                {name : 'Minnesota', value : 5379139},
+                {name : 'Mississippi', value : 2984926},
+                {name : 'Missouri', value : 6021988},
+                {name : 'Montana', value : 1005141},
+                {name : 'Nebraska', value : 1855525},
+                {name : 'Nevada', value : 2758931},
+                {name : 'New Hampshire', value : 1320718},
+                {name : 'New Jersey', value : 8864590},
+                {name : 'New Mexico', value : 2085538},
+                {name : 'New York', value : 19570261},
+                {name : 'North Carolina', value : 9752073},
+                {name : 'North Dakota', value : 699628},
+                {name : 'Ohio', value : 11544225},
+                {name : 'Oklahoma', value : 3814820},
+                {name : 'Oregon', value : 3899353},
+                {name : 'Pennsylvania', value : 12763536},
+                {name : 'Rhode Island', value : 1050292},
+                {name : 'South Carolina', value : 4723723},
+                {name : 'South Dakota', value : 833354},
+                {name : 'Tennessee', value : 6456243},
+                {name : 'Texas', value : 26059203},
+                {name : 'Utah', value : 2855287},
+                {name : 'Vermont', value : 626011},
+                {name : 'Virginia', value : 8185867},
+                {name : 'Washington', value : 6897012},
+                {name : 'West Virginia', value : 1855413},
+                {name : 'Wisconsin', value : 5726398},
+                {name : 'Wyoming', value : 576412},
+                {name : 'Puerto Rico', value : 3667084}
+            ]
+        }
+    ]
 };
 
-info.update = function (props) {
-	this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
-		'<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
-		: 'Hover over a state');
-};
+require(
+    [
+        'echarts',
+        'echarts/chart/map',
+    ],
+    function (ec) {
+        require('echarts/util/mapData/params').params.USA = {
+		    getGeoJson: function (callback) {
+		        $.getJSON('//ecomfe.github.io/echarts/doc/example/geoJson/USA_geo.json', callback);
+		    },
+		    specialArea : {
+		        Alaska : {              // 把阿拉斯加移到美国主大陆左下方
+		            left : -127,        // 安放位置起始点为西经113
+		            top : 30,           // 北纬31度
+		            width : 15,         // 区间大小宽高不大于15度
+		            height : 15
+		        },
+		        Hawaii : {
+		            left : -110,        // 夏威夷
+		            top : 28,
+		            width : 5,
+		            height : 5
+		        },
+		        'Puerto Rico' : {       // 波多黎各
+		            left : -76,
+		            top : 26,
+		            width : 2,
+		            height : 2
+		        }
+		    }
+		}
 
-info.addTo(map);
-
-
-// get color depending on population density value
-function getColor(d) {
-	return d > 1000 ? '#800026' :
-	       d > 500  ? '#BD0026' :
-	       d > 200  ? '#E31A1C' :
-	       d > 100  ? '#FC4E2A' :
-	       d > 50   ? '#FD8D3C' :
-	       d > 20   ? '#FEB24C' :
-	       d > 10   ? '#FED976' :
-	                  '#FFEDA0';
-}
-
-function style(feature) {
-	return {
-		weight: 2,
-		opacity: 1,
-		color: 'white',
-		dashArray: '3',
-		fillOpacity: 0.7,
-		fillColor: getColor(feature.properties.density)
-	};
-}
-
-function highlightFeature(e) {
-	var layer = e.target;
-
-	layer.setStyle({
-		weight: 5,
-		color: '#666',
-		dashArray: '',
-		fillOpacity: 0.7
-	});
-
-	if (!L.Browser.ie && !L.Browser.opera) {
-		layer.bringToFront();
-	}
-
-	info.update(layer.feature.properties);
-}
-
-var geojson;
-
-function resetHighlight(e) {
-	geojson.resetStyle(e.target);
-	info.update();
-}
-
-function zoomToFeature(e) {
-	map.fitBounds(e.target.getBounds());
-}
-
-function onEachFeature(feature, layer) {
-	layer.on({
-		mouseover: highlightFeature,
-		mouseout: resetHighlight,
-		click: zoomToFeature
-	});
-}
-
-geojson = L.geoJson(statesData, {
-	style: style,
-	onEachFeature: onEachFeature
-}).addTo(map);
-
-map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+        var myChart = ec.init(document.getElementById('map'));
 
 
-var legend = L.control({position: 'bottomright'});
+        myChart.setOption(option);
 
-legend.onAdd = function (map) {
 
-	var div = L.DomUtil.create('div', 'info legend'),
-		grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-		labels = [],
-		from, to;
-
-	for (var i = 0; i < grades.length; i++) {
-		from = grades[i];
-		to = grades[i + 1];
-
-		labels.push(
-			'<i style="background:' + getColor(from + 1) + '"></i> ' +
-			from + (to ? '&ndash;' + to : '+'));
-	}
-
-	div.innerHTML = labels.join('<br>');
-	return div;
-};
-
-legend.addTo(map);
+       
+    }
+);
